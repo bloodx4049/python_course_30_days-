15267388be84d9d64c4edcc8b1c93b60dc061950
bloodx4049 <awais.ahmad.1940@gmail.com>
2025-12-06 11:29:05 +0500

    added all files

---
+++ Chapter #9/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+line1 = f.readline()
+# print(line1, type(line1))
+
+# line2 = f.readline()
+# print(line2, type(line2))
+
+# line3 = f.readline()
+# print(line3, type(line3))
+
+# line4 = f.readline()
+# print(line4 =="")
